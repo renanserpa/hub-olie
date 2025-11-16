afe1b0257164fff59d2102daed247f58b8018a4b
renanserpa <39599731+renanserpa@users.noreply.github.com>
2025-11-16 15:28:59 -0400

    refactor: Clean up obsolete files and code
    Removes several obsolete files and code snippets that are no longer in use or have been migrated. This includes empty component files and hook definitions marked as obsolete, as well as minor formatting adjustments in other files.

---
+++ ProductsPage.tsx
@@ -0,0 +1,5 @@
+// This file is obsolete and has been emptied to resolve build errors.
+// The correct component is located at /components/ProductsPage.tsx
+import React from 'react';
+const ProductsPage: React.FC = () => null;
+export default ProductsPage;
---
+++ SettingsPage.tsx
@@ -0,0 +1,5 @@
+// This file is obsolete and has been emptied to resolve build errors.
+// The correct component is located at /components/SettingsPage.tsx
+import React from 'react';
+const SettingsPage: React.FC = () => null;
+export default SettingsPage;
