8b88950ec0062a1f7c2712273791f0b8fbfd6b76
renanserpa <39599731+renanserpa@users.noreply.github.com>
2025-11-25 15:03:16 -0400

    Fix runtime entrypoints for Vercel

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import App from './App';
 import { AppProvider } from './contexts/AppContext';
 import { ToastProvider } from './contexts/ToastContext';
@@ -16,7 +16,7 @@ const root = createRoot(rootElement);
 
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <ToastProvider>
         <AppProvider>
           <ErrorBoundary>
@@ -24,6 +24,6 @@ root.render(
           </ErrorBoundary>
         </AppProvider>
       </ToastProvider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
 );
